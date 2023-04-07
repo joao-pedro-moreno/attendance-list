a7c8d1daa188f017fe2516fdce657de2984489d7
João Pedro Moreno <devmoreno2003@gmail.com>
2023-04-07 17:43:24 -0300

    code refactored into typescript

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { Home } from './pages/Home'
 import './styles/global.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import './styles.css'
-import { Card } from '../../components/Card'
+import { Card, CardProps } from '../../components/Card'
+
+type APIResponse = {
+  name: string;
+  avatar_url: string;
+}
+
+type User = {
+  name: string;
+  avatar: string;
+}
 
 export function Home() {
   const [studentName, setStudentName] = useState('')
-  const [students, setStudents] = useState([])
-  const [user, setUser] = useState({ name: '', avatar: '' })
+  const [students, setStudents] = useState<CardProps[]>([])
+  const [user, setUser] = useState<User>({} as User)
 
   function handleAddStudent() {
     const newStudent = {
@@ -23,7 +33,7 @@ export function Home() {
   useEffect(() => {
     fetch("https://api.github.com/users/joao-pedro-moreno")
       .then(response => response.json())
-      .then(data => {
+      .then((data: APIResponse) => {
         setUser({
           name: data.name,
           avatar: data.avatar_url
